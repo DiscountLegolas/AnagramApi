01fe338e6e795eda720c2eb2660bc06334d8c43c
DiscountLegolas <69970115+DiscountLegolas@users.noreply.github.com>
2021-09-02 10:32:52 +0300

    English Anagrams Added

--- AnagramApi/Controllers/AnagramController.cs
+++ AnagramApi/Controllers/AnagramController.cs
@@ -11,24 +11,39 @@ using System.IO;
 
 namespace AnagramApi.Controllers
 {
+    [Produces("application/json")]
     [Route("[controller]")]
     [ApiController]
     public class AnagramController : ControllerBase
     {
+        /// <summary>
+        /// Fetches Anagrams Of The word in turkish
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     Get /Anagram/papatya
+        ///
+        /// </remarks>
+        /// <param name="kelime"></param>
         [Route("{kelime}")]
+        [HttpGet]
         public IActionResult Anagramlar(string kelime)
         {
             HttpClient client = new HttpClient();
             List<ResourceMadde> kelimeler = new List<ResourceMadde>();
-            var tümkelimeler = System.IO.File.ReadAllLines(@"C:\Users\prof1\OneDrive\Masaüstü\TDK_Sözlük_Kelime_Listesi.txt");
+            var tümkelimeler = System.IO.File.ReadAllLines("TDK_Sözlük_Kelime_Listesi.txt");
             foreach (var item in tümkelimeler.Where(x=>CheckİfCharlistContainsAnotherCharlist(kelime,x)&&x.Length>1&&x.ToLower()!=kelime.ToLower()))
             {
                 string uri = "https://sozluk.gov.tr/gts?ara=" + item;
-                var asstringresult = client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result;
-                var maddeler = JsonConvert.DeserializeObject<List<Madde>>(asstringresult);
-                foreach (var madde in maddeler)
+                if (client.GetAsync(uri).Result.IsSuccessStatusCode)
                 {
-                    kelimeler.Add(new ResourceMadde() { Kelime = madde.madde_duz, Anlamı =madde.anlamlarListe.First().anlam});
+                    var asstringresult = client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result;
+                    var maddeler = JsonConvert.DeserializeObject<List<Madde>>(asstringresult);
+                    foreach (var madde in maddeler)
+                    {
+                        kelimeler.Add(new ResourceMadde() { Kelime = madde.madde_duz, Anlamı = madde.anlamlarListe.First().anlam });
+                    }
                 }
             }
             for (int i = 0; i < kelimeler.Count; i++)
@@ -40,6 +55,37 @@ namespace AnagramApi.Controllers
                 }
             }
             return Ok(kelimeler);
+        }
+        [HttpGet]
+        [Route("English/{word}")]
+        public IActionResult Anagrams(string word)
+        {
+            HttpClient client = new HttpClient();
+            List<ResourceMadde> words = new List<ResourceMadde>();
+            var allwords= System.IO.File.ReadAllLines("words_alpha.txt");
+            foreach (var item in allwords.Where(x => CheckİfCharlistContainsAnotherCharlist(word, x) && x.Length > 1 && x.ToLower() != word.ToLower()))
+            {
+                string uri = "https://api.dictionaryapi.dev/api/v2/entries/en/" + item;
+                if (client.GetAsync(uri).Result.IsSuccessStatusCode)
+                {
+                    var asstringresult = client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result;
+                    var kelime = JsonConvert.DeserializeObject<List<Word>>(asstringresult).First();
+                    var meaning = "";
+                    foreach (var anlam in kelime.meanings)
+                    {
+                        if (kelime.meanings.Last()!=anlam)
+                        {
+                            meaning = meaning + anlam.definitions.First().definition + ",";
+                        }
+                        else
+                        {
+                            meaning = meaning + anlam.definitions.First().definition;
+                        }
+                    }
+                    words.Add(new ResourceMadde() { Kelime = kelime.word, Anlamı = meaning });
+                }
+            }
+            return Ok(words);
         }
         private bool CheckİfCharlistContainsAnotherCharlist(string  word,string  word2)
         {
